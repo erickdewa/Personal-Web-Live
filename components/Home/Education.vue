baa644f8dc557269ab72f182892b32209e1bb4d6
erickdewa <pranatau11012003@gmail.com>
2022-05-31 21:27:20 +0700

    education dark support

--- components/Home/Education.vue
+++ components/Home/Education.vue
@@ -8,12 +8,10 @@
                     <svg style="position: absolute; right: -10px; bottom: -5px; width: 200px; transform: rotate(3deg);" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 416.04 71.16"><path d="M788.6,61.29l-83.81.14c-26.91,0-53.72.33-80.5,3.34S571.1,72,544.64,76.33c-25.31,4.15-50.55,8.85-75.51,14.81a572.41,572.41,0,0,0-83.8,26.45c-3.73,1.52-6.42,5-5.24,9.22,1,3.55,5.44,6.79,9.23,5.24,47.8-19.53,98.34-30.9,149.12-39.55,25.62-4.37,51.35-8.45,77.15-11.69,26.7-3.35,53.38-4.3,80.28-4.36q46.36-.12,92.73-.16C798.25,76.27,798.27,61.27,788.6,61.29Z" transform="translate(-379.81 -61.29)" style="fill:#50b792"/></svg>
                 </div>
                 <div class="education-ui-circle"></div>
-                <div class="education-ui-dot top d-block"></div>
-                <div class="education-ui-dot bottom d-block"></div>
             </div>
             <div class="education-list">
                 <div class="education-item">
-                    <div class="education-box py-3 px-4">
+                    <div class="education-box py-3 px-4 cursor-pointer">
                         <div class="education-box-dash" style="width: 255px; left: -255px;"></div>
                         <div class="education-box-dot"></div>
                         <div class="education-tipe">Middle School</div>
@@ -22,7 +20,7 @@
                     </div>
                 </div>
                 <div class="education-item">
-                    <div class="education-box py-3 px-4">
+                    <div class="education-box py-3 px-4 cursor-pointer">
                         <div class="education-box-dash" style="width: 200px; left: -200px;"></div>
                         <div class="education-box-dot"></div>
                         <div class="education-tipe">High School</div>
@@ -31,7 +29,7 @@
                     </div>
                 </div>
                 <div class="education-item">
-                    <div class="education-box py-3 px-4">
+                    <div class="education-box py-3 px-4 cursor-pointer">
                         <div class="education-box-dash" style="width: 255px; left: -255px;"></div>
                         <div class="education-box-dot"></div>
                         <div class="education-tipe">University</div>
@@ -56,6 +54,9 @@
         opacity: 0.5;
         border-radius: 0% 100% 100% 0%;
     }
+    .dark-web .decore-edication{
+        background-image: url('~/assets/images/shape/shape-bg-dark-1.png');
+    }
     .education-ui-box{
         display: flex;
         align-items: center;
@@ -69,6 +70,9 @@
         font-weight: bolder;
         color: #000000;
     }
+    .dark-web .education-ui-box .title-text{
+        color: #ffffff;
+    }
     .education-ui-box .education-ui-circle{
         position: absolute;
         right: 0px;
@@ -78,22 +82,6 @@
         border-radius: 50%;
         border: 2px solid #23cf9f;
         z-index: 1;
-    }
-    .education-ui-box .education-ui-dot{
-        background: #23cf9f;
-        position: absolute;
-        width: 13px;
-        height: 13px;
-        border-radius: 50%;
-        z-index: 20;
-    }
-    .education-ui-box .education-ui-dot.top{
-        top: 0px;
-        right: 24%;
-    }
-    .education-ui-box .education-ui-dot.bottom{
-        bottom: 0px;
-        right: 24%;
     }
 
     .education-list{
@@ -109,6 +97,9 @@
         min-width: 350px;
         margin-left: 200px;
         border-radius: 5px;
+    }
+    .dark-web .education-list .education-box{
+        background: #565656;
     }
     .education-box .education-box-dot{
         background: #23cf9f;
@@ -137,8 +128,14 @@
         font-size: 18px;
         font-weight: 600;
     }
+    .dark-web .education-box .education-school{
+        color: #ffffff;
+    }
     .education-box .education-year{
         color: #000000;
         font-size: 15px;
+    }
+    .dark-web .education-box .education-year{
+        color: #d9d9d9;
     }
 </style>
